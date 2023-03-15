af24505f9dee3197a56e0309e2703e3a3ea1c374
stickwithme <67421766+stickwithme@users.noreply.github.com>
2023-03-15 04:30:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// Получаем элементы калькулятора
+
 const calculator = document.querySelector('.calculator')
 const display = calculator.querySelector('.calculator__display')
 const buttons = calculator.querySelectorAll('.calculator__button')
 
-// Создаем переменные для хранения значений
+
 let currentOperand = ''
 let previousOperand = ''
 let operation = undefined
 
-// Функция для обновления значения на дисплее
+
 function updateDisplay() {
   display.innerText = currentOperand
 }
 
-// Функция для добавления числа или точки
+
 function appendNumber(number) {
   if (number === '.' && currentOperand.includes('.')) return
   currentOperand = currentOperand.toString() + number.toString()
   updateDisplay()
 }
 
-// Функция для выбора операции
+
 function chooseOperation(selectedOperation) {
   if (currentOperand === '') return
   if (previousOperand !== '') {
@@ -31,7 +31,7 @@ function chooseOperation(selectedOperation) {
   currentOperand = ''
 }
 
-// Функция для вычисления результата
+
 function compute() {
   let result
   const prev = parseFloat(previousOperand)
@@ -58,7 +58,7 @@ function compute() {
   previousOperand = ''
 }
 
-// Функция для очистки калькулятора
+
 function clear() {
   currentOperand = ''
   previousOperand = ''
@@ -66,7 +66,7 @@ function clear() {
   updateDisplay()
 }
 
-// Добавляем обработчики событий для кнопок калькулятора
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const value = button.dataset.value
